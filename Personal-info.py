bcae03f3335b488ed9922fd2cca5db6d0aeeca84
roibermudez <82429481+roibermudez@users.noreply.github.com>
2021-05-18 16:09:46 -1200

    Create Personal-info.py

---
+++ Personal-info.py
@@ -0,0 +1,6 @@
+firstName = input("What is your Name? ")
+lastName = input("What is your LastName? ")
+yourlocation = input("What is your Location? ")
+yourAge = input("What is your Age? ")
+space = " "
+print("Hi" + firstName + space + lastName + "!" + space + "Your location is" + yourLocation + "and you are" + yourAge + "years old")
